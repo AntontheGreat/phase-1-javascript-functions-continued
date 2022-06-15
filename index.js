0823dd63139ff272d71837b10014de8d8cf6541c
Antonio <antonio18casta@aol.com>
2022-06-15 17:23:14 -0400

    commit tree 8be6a4892205a226703580863aebd48589d94435

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function razzle() {
+    console.log("You've been razzled");
+}
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(str) {
+    return function(name =  "special") {
+        return `You are ${str}${name}${str}!`
+    }
+}
